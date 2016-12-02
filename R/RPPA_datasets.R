e53370fedb88f8e3278cb66ff44a5ce7e2b3a71d
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2016-12-02 15:12:16 +0100

    ver 0.99.0 - init data

---
+++ R/RPPA_datasets.R
@@ -0,0 +1,186 @@
+#' 
+#' RPPA datasets from TCGA project from 2016-01-28 release date
+#'
+#' Package provides RPPA datasets from The Cancer Genome Atlas Project for all cohorts types from \url{http://gdac.broadinstitute.org/}.
+#' Data were downloaded using \link{RTCGA-package} and contain snapshots for the release date: \code{2016-01-28} . Visit \pkg{RTCGA} site: \url{http://rtcga.github.io/RTCGA/}.
+#' Use cases, examples and information about datasets in \pkg{RTCGA.RPPA.20160128}  can be found here: \code{browseVignettes("RTCGA")}. Link to the data format explanation is in the package DESCRIPTION file.
+#' 
+#' @details \code{browseVignettes("RTCGA")}
+#' 
+#' @param metadata A logical indicating whether load data into the workspace (default, \code{FALSE}) or to only display the object's metadata (\code{TRUE}). See examples.
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#'  ACC.RPPA.20160128(metadata = TRUE) 
+#'  ACC.RPPA.20160128(metadata = FALSE) 
+#'  ACC.RPPA.20160128 
+#' }
+#' 
+#' @import RTCGA
+#' @import ExperimentHub
+#' @importFrom utils read.csv
+#' @importFrom AnnotationHub query
+#' @format NULL
+#' @source \url{http://gdac.broadinstitute.org/}
+#' 
+#' @aliases ACC.RPPA.20160128,BLCA.RPPA.20160128,BRCA.RPPA.20160128,CESC.RPPA.20160128,CHOL.RPPA.20160128,COADREAD.RPPA.20160128,DLBC.RPPA.20160128,ESCA.RPPA.20160128,GBMLGG.RPPA.20160128,HNSC.RPPA.20160128,KICH.RPPA.20160128,KIPAN.RPPA.20160128,KIRC.RPPA.20160128,KIRP.RPPA.20160128,LIHC.RPPA.20160128,LUAD.RPPA.20160128,LUSC.RPPA.20160128,MESO.RPPA.20160128,OV.RPPA.20160128,PAAD.RPPA.20160128,PCPG.RPPA.20160128,PRAD.RPPA.20160128,SARC.RPPA.20160128,SKCM.RPPA.20160128,STAD.RPPA.20160128,STES.RPPA.20160128,TGCT.RPPA.20160128,THCA.RPPA.20160128,THYM.RPPA.20160128,UCEC.RPPA.20160128,UCS.RPPA.20160128,UVM.RPPA.20160128 
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#'
+"ACC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"BLCA.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"BRCA.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"CESC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"CHOL.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"COADREAD.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"DLBC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"ESCA.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"GBMLGG.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"HNSC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"KICH.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"KIPAN.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"KIRC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"KIRP.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"LIHC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"LUAD.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"LUSC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"MESO.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"OV.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"PAAD.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"PCPG.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"PRAD.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"SARC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"SKCM.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"STAD.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"STES.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"TGCT.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"THCA.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"THYM.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"UCEC.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"UCS.RPPA.20160128"
+#' @name RPPA.20160128 
+#' @rdname RPPA.20160128 
+#' @format NULL 
+#'
+"UVM.RPPA.20160128"
---
+++ inst/scripts/make-data.R
@@ -0,0 +1,2 @@
+library(RTCGA)
+ createTCGA(description = system.file("DESCRIPTION", package = "RTCGA.RPPA.20160128"),      clean = TRUE)
